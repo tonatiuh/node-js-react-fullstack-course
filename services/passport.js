585910e9d9bdcbe575fef4ed1ce9a1b5940ba423
Tonatiuh Núñez <tonatiuh.nunez@ooma.com>
2019-04-21 09:00:41 -0500

    add mongoose; create User model; add googleId to User on login

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -9,9 +12,9 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
+    async (accessToken, refreshToken, profile, done) => {
       console.log('profile', profile);
+      const x = await new User({ googleId: profile.id }).save();
+      console.log('x', x);
     })
 );
